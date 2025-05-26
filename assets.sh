db3050377d8234c53ec18ab42082ff5669fa65ae
Humberto <hldtux@gmail.com>
2025-05-26 18:53:49 -0300

    verbose

--- assets.sh
+++ assets.sh
@@ -9,14 +9,14 @@ for cmd in curl unzip mkdir; do
 done
 
 # Define the games and base URL
-curl -ks -o sf2.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/java-fighting-game-engine/sf2_images_0.1.zip
-unzip -oq sf2.zip -d "org.jfge.games.sf2/assets"
+curl -k -o sf2.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/java-fighting-game-engine/sf2_images_0.1.zip
+unzip -o sf2.zip -d "org.jfge.games.sf2/assets"
 rm -f sf2.zip
 
-curl -ks -o mk2.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/java-fighting-game-engine/mk2_images_0.1.zip
-unzip -oq mk2.zip -d "org.jfge.games.mk2/assets"
+curl -k -o mk2.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/java-fighting-game-engine/mk2_images_0.1.zip
+unzip -o mk2.zip -d "org.jfge.games.mk2/assets"
 rm -f mk2.zip
 
-curl -ks -o sfvsmk2.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/java-fighting-game-engine/sfvsmk2_images_0.1.zip
-unzip -oq sfvsmk2.zip -d "org.jfge.games.sfvsmk2/assets"
+curl -k -o sfvsmk2.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/java-fighting-game-engine/sfvsmk2_images_0.1.zip
+unzip -o sfvsmk2.zip -d "org.jfge.games.sfvsmk2/assets"
 rm -f sfvsmk2.zip
