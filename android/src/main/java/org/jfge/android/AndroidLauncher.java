faa07bac51d124be5f2a1dd4c643e5fb4bf26ea0
Humberto <hldtux@gmail.com>
2026-07-25 18:17:55 -0300

    Fix Android launchers for libGDX 1.14 immersive mode API change.

--- android/src/main/java/org/jfge/android/AndroidLauncher.java
+++ android/src/main/java/org/jfge/android/AndroidLauncher.java
@@ -13,7 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
     super.onCreate(savedInstanceState);
 
     AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-    config.useImmersiveMode(true);
     config.useAccelerometer = false;
     config.useCompass = false;
 
--- android/src/main/java/org/jfge/android/Mk2AndroidLauncher.java
+++ android/src/main/java/org/jfge/android/Mk2AndroidLauncher.java
@@ -12,7 +12,6 @@ public class Mk2AndroidLauncher extends AndroidApplication {
     super.onCreate(savedInstanceState);
 
     AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-    config.useImmersiveMode(true);
     config.useAccelerometer = false;
     config.useCompass = false;
 
